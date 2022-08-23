187d2040ef9f1a53e6934f62030a94bbd3aa8fe8
Anni0007 <anirudh.baghel@konverge.ai>
2022-08-23 10:52:40 +0530

    black jack game completed

--- black_jack_game/index.js
+++ black_jack_game/index.js
@@ -1,20 +1,62 @@
 let sum = 0
-let cards = [1,2,3]
+let cards = []
+let hasBlackJack =  false
+let isAlive = false
+let message = "Want to play a round?"
 
 cardEl = document.getElementById("card-element")
-
-cardStr = ""
-
-for(let i = 0; i < cards.length; i++){
-    cardStr += cards[i] + "-"
-}
-
-cardEl.textContent += cardStr
+messageEl = document.getElementById("message-element")
+sumEl = document.getElementById("sum-element")
+messageEl.textContent = message
 
 function startGame(){
-
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard , secondCard]
+    sum = firstCard + secondCard
+    renderGame()
 }
 
-function newCard(){
+function renderGame(){
+    cardEl.textContent = "Cards:"
 
+    for(let i = 0; i < cards.length; i++){
+        cardEl.textContent += cards[i] + "  "
+    }
+
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20){
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+
+function newCard(){
+    if(isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
+}
+
+function getRandomCard(){
+  let randomNumber = Math.floor(Math.random() * 13  ) + 1
+    if (randomNumber > 10) {
+        return 10
+    }
+    else if (randomNumber === 1) {
+        return 11
+    }
+    else {
+        return randomNumber
+    }
 }
