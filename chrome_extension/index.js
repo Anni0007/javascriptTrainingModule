b79be4e9bfaed70e9442e39d2a225dc65ca3cb57
Anni0007 <anirudh.baghel@konverge.ai>
2022-08-24 23:02:45 +0530

    Chrome Extension completed

--- chrome_extension/index.js
+++ chrome_extension/index.js
@@ -2,26 +2,47 @@ let myLeads = []
 const inputEl = document.getElementById('input-el')
 const inputButtonEl = document.getElementById('input-btn')
 const leadsListElement = document.getElementById('show-leads-list')
+const delButtonEL = document.getElementById("del-btn")
+const tabButtonEL = document.getElementById("tab-btn")
+
 
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
 if (leadsFromLocalStorage){
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    renderLeads(myLeads)
 }
+
 inputButtonEl.addEventListener('click', function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
+    renderLeads(myLeads)
 })
 
-function renderLeads() {
+delButtonEL.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    renderLeads(myLeads)
+})
+
+tabButtonEL.addEventListener('click', function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tab) {
+        myLeads.push(tab[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        renderLeads(myLeads)
+    })
+    console.log("button clicked")
+})
+
+
+function renderLeads(leadArr) {
     let listItems = ""
 
-    for( let i = 0; i < myLeads.length; i++){
+    for( let i = 0; i < leadArr.length; i++){
         listItems += `<li> 
-                        <a href='${myLeads[i]}'target="_blank">
-                            ${myLeads[i]}
+                        <a href='${leadArr[i]}'target="_blank">
+                            ${leadArr[i]}
                         </a>
                      </li>`
     }
